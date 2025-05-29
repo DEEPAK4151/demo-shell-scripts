87677497ce9d650793f33596118e40e644b8ce91
deepak__4151 <deepakpateliit21@gmail.com>
2025-05-29 06:26:40 +0000

    added both files

---
+++ hello.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+echo "adarniya sabhapati mohadaya"
+echo " chatur : aj hum sab mil ke chamatkar krenge"
+
+mkdir -p folder
+echo "abba nhi manage" > text.txt
+#arguments 
+ 
+
+echo " current logged in user $USER"
+#user input
+
+read -p " rancho ka pura naam kya ha" fullname
+echo " rancho ka pura naam $fullname tha"
+
+#arguments
+
+#./hello.sh raju farahan rancho
+
+echo "movie ka naam $0"
+echo " first idiot $1"
+echo "second idiot $2"
